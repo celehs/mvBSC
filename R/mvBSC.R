96b30ae55b0d6eb43a9370d87f3a3b0c4c15afe5
Ming Yang <hustyangming@gmail.com>
2019-07-24 19:42:45 +0800

    update

--- R/mvBSC.R
+++ R/mvBSC.R
@@ -1,3 +1,6 @@
+#' @import stats
+NULL
+
 #' @title ...
 #' @param cosK ...
 #' @param h ...
@@ -23,4 +26,91 @@ get_Z <- function(codes, labels) {
   rownames(Z) <- codes
   colnames(Z) <- uniqueLabels
   return(Z)
+}
+
+#' @title ...
+#' @param Z1 ...
+#' @param Z2 ...
+#' @param digits ...
+#' @export
+NMI <- function(Z1, Z2, digits = 3) {
+  Z1 <- as.matrix(Z1) 
+  Z2 <- as.matrix(Z2)
+  n <- nrow(Z1)
+  A <- t(Z1) %*% Z2
+  probA = A / sum(A) #joint prob
+  probZ1 = apply(probA, 1, sum)
+  probZ2 = apply(probA, 2, sum)
+  probB = log(probA / (probZ1 %*% t(probZ2)))
+  probB[is.infinite(probB)] <- 0
+  top <- sum(probA * probB)
+  bottom <- sqrt(t(probZ1) %*% log(probZ1) * t(probZ2) %*% log(probZ2))
+  nmi <- as.numeric(top / bottom)
+  return(round(nmi, digits = digits))
+}
+
+#' @title ...
+#' @param Z1 ...
+#' @param Z2 ...
+#' @param digits ...
+#' @export
+ARI <- function(Z1, Z2, digits = 3) {
+  n <- nrow(Z1)
+  contingencyTb <- t(Z1) %*% Z2
+  one <- rep(1, n)
+  a <- t(Z1) %*% one
+  b <- t(Z2) %*% one
+  A <- 0.5 * sum(apply(contingencyTb, c(1, 2), function(x) x^2 - x)) 
+  B <- 0.25 * sum(a*(a-1))*sum(b*(b-1))
+  C <- 0.5 * n * (n - 1)
+  D <- 0.25 * (sum(a * (a - 1)) + sum(b * (b - 1)))
+  ARI <- (A - B / C) / (D - B / C)
+  return(round(ARI, digits = digits))
+}
+
+#' @title ...
+#' @param Z ...
+#' @param Z0 ...
+#' @param digits ...
+#' @export
+F_measure <- function(Z, Z0, digits = 3) {
+  PZ <- Z %*% t(Z)
+  PZ0 <- Z0 %*% t(Z0)
+  precision <- round(sum(PZ * PZ0) / sum(PZ), digits = digits)
+  recall <- round(sum(PZ * PZ0) / sum(PZ0), digits = digits)
+  f_measure <- round(2 * precision * recall / (precision + recall), digits = digits)
+  return(c("precision" = precision, "recall" = recall, "f" = f_measure))
+}
+
+#' @title ...
+#' @param codes ...
+#' @param wt ...
+#' @param cosK ...
+#' @param h ...
+#' @param k ...
+#' @param R ...
+#' @param Z0 ...
+#' @param seed ...
+#' @param ... ...
+#' @export
+mvbsc <- function(codes, wt, cosK, h, k, R, Z0, seed = 123, ...) {
+  W <- 0
+  for (i in 1:length(wt)) {
+    if (wt[i] > 0) {
+      tmp <- get_U(cosK = cosK[[i]], h = h[i], k = k, R = R)
+      W <- W + wt[i] * tcrossprod(tmp)
+    }
+  }
+  eigen_W <- eigen(W)
+  idx <- order(abs(eigen_W$values), decreasing = TRUE)
+  U <- eigen_W$vectors[, idx[1:k]]
+  rownames(U) <- rownames(W)
+  set.seed(seed)
+  kmeans_res <- kmeans(U, k, iter.max = 100, nstart = 25, ...)
+  Z <- get_Z(codes, kmeans_res$cluster[codes])
+  list(kmeans_res, 
+       metrics = c(
+         ARI = ARI(Z, Z0), 
+         NMI = NMI(Z, Z0), 
+         f_measure = F_measure(Z, Z0)))
 }
